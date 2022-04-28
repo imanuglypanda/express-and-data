456b69480e35e2048a089bf589345a3f556dfc79
Tanapatjeensamran <imanuglypanda@gmail.com>
2022-04-28 12:45:40 +0700

    1

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ app.use(express.json());
 // Database
 require('./database');
 
+// studentRouter is DataBase name
+const studentRouter = require('./routes/api/students');
+app.use('/api/students', studentRouter)
+
+
 // Path html
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// studentRouter is DataBase name
-const studentRouter = require('./routes/api/api');
-app.use('/api', studentRouter)
 
 // Listen
 app.listen(PORT, () => console.log(`Server is runnung on port ${PORT}`));
